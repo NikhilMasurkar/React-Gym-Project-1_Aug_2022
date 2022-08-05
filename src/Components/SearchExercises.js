0e5b8276bd17e9ed1f72777ad3dcf06bd77aa648
NikhilMasurkar <101415426+NikhilMasurkar@users.noreply.github.com>
2022-08-06 02:41:03 +0530

    Update SearchExercises.js

--- src/Components/SearchExercises.js
+++ src/Components/SearchExercises.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
-import HorizontalScrollbar from './HorizontalScrollbar';
+import HorizontalScrollbar from './HorizontalScrollBar';
 
 //get the props from the home component.
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
